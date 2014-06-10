de0bb91d56c5b93d6adbe8bff48ec7b5de046e03
Arjun <arjun.bali96@gmail.com>
2014-06-10 15:31:15 -0400

    created contact class

--- crm.rb
+++ crm.rb
@@ -1,6 +1,23 @@
 require 'sinatra'
+require_relative 'contact'
 
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+#Add a new contacts
+get '/contacts/new' do
+end
+
+#View a contacts
+get '/contacts/:id' do
+end
+
+#modify an existing ocntact
+get '/contacts/:id/edit' do
 end
