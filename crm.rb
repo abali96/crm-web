db10f0d05f2c7800bcc000d2bc3dadf296f04a66
Arjun <arjun.bali96@gmail.com>
2014-06-12 14:03:22 -0400

    added find contact feature

--- crm.rb
+++ crm.rb
@@ -2,7 +2,8 @@ require 'sinatra'
 require_relative 'contact'
 require_relative 'rolodex'
 
-@@rolodex = Rolodex.new("AHAHAH")
+@@rolodex = Rolodex.new
+@@rolodex.add_contact(Contact.new("jonny", "appleseed", "jonny@bitmakerlabs.com", "generic character"))
 
 
 get '/' do
@@ -20,15 +21,21 @@ get '/contacts/new' do
 end
 
 #View a contacts
-get '/contacts/:id' do
+# get '/contacts/:id' do
+# end
+
+get "/contacts/1" do
+	@contact = @@rolodex.find(1)
+	erb :show_contact
 end
 
-#modify an existing ocntact
+# modify an existing ocntact
 get '/contacts/:id/edit' do
 end
 
 #post request for submission form
 post '/contacts' do
+	puts params
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	@@rolodex.add_contact(new_contact)
 	redirect('/contacts')
--- rolodex.rb
+++ rolodex.rb
@@ -1,16 +1,19 @@
 class Rolodex
 	attr_reader :contacts
 
-	def initialize(name)
+	def initialize
 		@contacts = []
 		@contact_id = 1
-		@name = name
 	end
 
 	def add_contact(contact)
 		contact.id = @contact_id
 		@contacts << contact
 		@contact_id += 1
+	end
+
+	def find(contact_id)
+		@contacts.find { |contact| contact.id == contact_id }
 	end
 
 end
