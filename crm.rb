af6c0f25918a7a1c073a513ae367b245683f0ce9
Arjun <arjun.bali96@gmail.com>
2014-06-13 13:39:02 -0400

    CRUD COMPLETE

--- crm.rb
+++ crm.rb
@@ -16,10 +16,6 @@ end
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
-# FAKE DATA:
-# @@rolodex.add_contact(Contact.new("jonny", "appleseed", "jonny@bitmakerlabs.com", "generic character"))
-
-
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
@@ -48,7 +44,7 @@ end
 
 # modify an existing contact
 get '/contacts/:id/edit' do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id].to_i)
 	if @contact
 		erb :edit_contact
 	else
@@ -68,13 +64,13 @@ post '/contacts' do
 end
 
 put '/contacts/:id' do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id].to_i)
 	if @contact
 		@contact.first_name = params[:first_name]
 		@contact.last_name = params[:last_name]
 		@contact.email = params[:email]
 		@contact.note = params[:note]
-
+		@contact.save
 		redirect to("/contacts")
 	else 
 		raise Sinatra::NotFound
@@ -83,12 +79,6 @@ end
 
 delete "/contacts/:id" do
 	@contact = Contact.get(params[:id].to_i)
-	@contact.destroy
-	# if @contact
-	# 	@@rolodex.remove_contact(@contact)
-	# 	redirect to ("/contacts")
-	# else
-	# 	raise Sinatra::NotFound
-	# end		
+	@contact.destroy	
 	redirect to("/contacts")
 end
